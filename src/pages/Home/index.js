7ba6780d99899dc421408ab342618e69acd9d67e
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-04-03 17:41:57 -0300

    [add] test deploy

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Product from '../../components/Product/Product'
 import Cart from '../../components/Cart/Cart'
+import getProducts from '../../services/products'
 
 const App = ({user}) => {
   const [products, setProducts] = useState()
   const [cart, setCart] = useState([])
 
   useEffect( () => {
-      fetch(`${process.env.REACT_APP_API_URL}/products`, {
-        'mode': 'cors',
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-        }
-      })
-      .then(res => {
-        console.log(res.json())
-        res.json()})
-      .then(data => setProducts(data.products))
-  }, [products])
+      const getProductss = async () => {
+        const data = await getProducts()
+        setProducts(data.products)
+      }
+      getProductss()
+  }, [])
 
   return ( 
     <div className="flex m-auto py-6 justify-center px-12">
--- src/services/updateUser.js
+++ src/services/updateUser.js
@@ -11,7 +11,7 @@ const updateUser = async (token, data) => {
 
   try{
     const res = await fetch(`${process.env.REACT_APP_API_URL}/user/me`, requestOptions)
-    const userData = await res.json();
+    const userData = await res.json()
     return userData
   } catch (err) {
     return err
